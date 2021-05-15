84384792f1bebc4b0a3254743c5756be03a6c31b
Royston Dsouza <goodwillwins@gmail.com>
2021-05-15 15:27:38 +0530

    config files

--- svr/myconfig.js
+++ svr/myconfig.js
@@ -1,5 +1,5 @@
 module.exports = {
-    serveraddress : "http://192.168.0.12:4004",
+    serveraddress : "http://192.168.0.12:4004", /*ip of the nodesvr; the iot devices will ping this*/
     serverPort : 4004,
     iotwebsockport: 2457,
 }
